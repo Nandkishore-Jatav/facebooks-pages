8a4456a52f9c6b857847a65fff39da54b69353cc
Nandkishore-Jatav <88927098+Nandkishore-Jatav@users.noreply.github.com>
2021-09-12 22:57:03 +0530

    Add files via upload

---
+++ 01_facebook-pages/03_facebook.js
@@ -0,0 +1,22 @@
+// function clicks01(){
+//     let elements = document.getElementById("change-text");
+//     elements.style.color = red;
+// }
+
+function mOver(obj) {
+  obj.style.color = "orange";
+}
+
+function mOut(obj) {
+  obj.style.color = "#415e9b";
+}
+
+function mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+    // obj.innerHTML = "Release Me";
+  }
+  
+  function mUp(obj) {
+    obj.style.backgroundColor="#866af7";
+    // obj.innerHTML="Thank You";
+  }
